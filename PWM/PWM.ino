b6662a8f5943cebe39581d45818bacfdd10fb810
rondayvoo <rondayvoo911@gmail.com>
2021-02-25 19:28:16 +0800

    Updated forward/reverse timers

--- PWM/PWM.ino
+++ PWM/PWM.ino
@@ -12,19 +12,28 @@
 #define DUTY 200
 
 void setup() {
-  DDRD |= (PIN5);
-  DDRB |= (PIN3);
+  DDRD |= (PIN5 | PIN6);
+  DDRB |= (PIN2 | PIN3);
   TCNT0 = 0;
+  TCNT1 = 0;
   TCNT2 = 0;
-  TCCR0A = 0b00100001;
+  TCCR0A = 0b10100001; 
+  TCCR1A = 0b00100011; 
   TCCR2A = 0b10000001;
-  TIMSK0 |= 0b110; 
-  TIMSK2 |= 0b110;
+  TIMSK0 |= 0b011; 
+  TIMSK1 |= 0b100;
+  TIMSK2 |= 0b100;
+
+  //Uncomment the next two lines to move forward 
   //OCR0A = DUTY;
-  OCR0B = DUTY;
-  OCR2A = DUTY;
-  //OCR2B = DUTY;
+  //OCR1B = DUTY << 8;
+
+  //Uncomment the next two lines to move in reverse
+  //OCR0B = DUTY;
+  //OCR2A = DUTY;
+  
   TCCR0B = 0b00000011;
+  TCCR1B = 0b00010011;
   TCCR2B = 0b00000100;
   sei();
 }
@@ -34,6 +43,14 @@ ISR(TIMER0_COMPA_vect)
 }
 
 ISR(TIMER0_COMPB_vect)
+{
+}
+
+ISR(TIMER1_COMPB_vect)
+{
+}
+
+ISR(TIMER2_COMPA_vect)
 {
 }
 
